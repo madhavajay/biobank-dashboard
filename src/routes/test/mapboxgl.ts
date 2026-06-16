5ac0118a202c437092903c5be0a165e9348eecfe
keelan <kj@kj.dev>
2026-06-16 19:31:55 +0100

    bundle worker

--- src/routes/test/mapboxgl.ts
+++ src/routes/test/mapboxgl.ts
@@ -1,8 +1,11 @@
 import mapboxgl from 'mapbox-gl'
+import mapboxWorkerUrl from 'mapbox-gl/dist/mapbox-gl-csp-worker.js?url'
 
 export const key = Symbol('mapbox')
 
 mapboxgl.accessToken =
 	'pk.eyJ1Ijoia2VlbGFuam9yZGFuIiwiYSI6ImNtcWd5cDM3ZTA0emUycHNnb3JucmgwdDkifQ.ycaW_8e4TyKhAy-aikzXbw'
+
+mapboxgl.workerUrl = mapboxWorkerUrl
 
 export { mapboxgl }
